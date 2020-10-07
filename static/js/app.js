971de282f0923ae92894ee2761eaa629ec66975c
MichelleH <voirdire96@gmail.com>
2020-10-06 20:43:44 -0700

    Json data test

--- static/js/app.js
+++ static/js/app.js
@@ -1,157 +1,159 @@
-let person = {"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0};
-var otu_ids = [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342];
-var sample_values = [163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-var otu_labels = ["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Proteobacteria;Epsilonproteobacteria;Campylobacterales;Campylobacteraceae;Campylobacter", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae;Varibaculum", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Veillonellaceae", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Peptococcaceae;Peptococcus", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae;Prevotella", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria", "Bacteria;Firmicutes", "Bacteria", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria"];
+d3.json('samples.json').then(data => {
+    console.log(data);
+});
 
-function init() {
-    // Make the bar chart of the top ten bacteria from the person ID
-    // Slice the first 10 objects for plotting
-    let slicedData = sample_values.slice(0, 10);
-    let slicedIDs = otu_ids.slice(0, 10);
-
-    // Reverse the array to accommodate Plotly's defaults
-    let reversedData = slicedData.reverse();
-    let reversedIDs = slicedIDs.reverse();
-    reversedIDs = reversedIDs.map(id=>"OTU " + id.toString());
-
-    // Trace for the Bar Data
-    var barTrace = {
-        x: reversedData,
-        y: reversedIDs,
-        text: reversedIDs,
-        name: "BB Bacteria",
-        type: "bar",
-        orientation: "h"
-    };
-    
-    // Bar Data
-    var barData = [barTrace];
-    
-    // Apply the group bar mode to the layout
-    var barLayout = {
-        title: "Belly Button Bacteria",
-    };
-
-   // Make the bubble chart of the top ten bacteria  from the person ID
-   
-    var bubbleTrace = {
-        x: otu_ids,
-        y: sample_values,
-        text: otu_labels,
-        mode: 'markers',
-        marker: {
-        color: otu_ids,
-        size: sample_values
-        }
-    };
-    
-    var bubbleData = [bubbleTrace];
-    
-    var bubbleLayout = {
-        title: 'Marker Size',
-        showlegend: false,
-        height: 600,
-        width: 600
-    };
-
-    Plotly.newPlot("bar", barData, barLayout);
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-};
-
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-// * Use `sample_values` as the values for the bar chart.
-
-// * Use `otu_ids` as the labels for the bar chart.
-
-// * Use `otu_labels` as the hovertext for the chart.
-
-// On change to the DOM, call getData()
-// d3.selectAll("#selDataset").on("change", getData);
-
-// function getData() {
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var personID = dropdownMenu.property("value");
-
-//     // Initialize an empty array for the person ID data
-//     var personDataset = [];
-
-// List of meta data for the selected person ID
-for (const [key, value] of Object.entries(person)) {
-    d3.select("ul").append("li").text(`${key}: ${value}`);
-  }
-  
-
-
-
-// // Sort the array in descending order
-// var numArray = [10, 22, 4];
-// numArray.sort(function compareFunction(a, b) {
-//   // resulting order is descending
-//   return b - a;
-// });
-
-// // Return elements from indicies 0 to 9
-// var topTen = bacteria.slice(0, 10);
-// // Reverse the list for plotly to order corectly
-// topTen.reverse();
-// console.log(topTen);
-
-//   // Call functions to update the charts
-//     updateBar(personDataset);
-//     updateBubble(personDataset);
-
-// };
-
-
-// // Update the restyled bar plot's values
-// function updateBar(newdata) {
-//     Plotly.restyle("bar", "values", [newdata]);
-//   }
-// // Update the restyled bubble plot's values
-// function updateBubble(newdata) {
-//     Plotly.restyle("bubble", "values", [newdata]);
-//   }
-
-// };
-
+// let person = {"id": 940, "ethnicity": "Caucasian", "gender": "F", "age": 24.0, "location": "Beaufort/NC", "bbtype": "I", "wfreq": 2.0};
+// var otu_ids = [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342];
+// var sample_values = [163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+// var otu_labels = ["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Proteobacteria;Epsilonproteobacteria;Campylobacterales;Campylobacteraceae;Campylobacter", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae;Varibaculum", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Veillonellaceae", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Peptococcaceae;Peptococcus", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae;Prevotella", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria", "Bacteria;Firmicutes", "Bacteria", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria"];
 
 // function init() {
-//     data = [{
-//       x: [1, 2, 3, 4, 5],
-//       y: [1, 2, 4, 8, 16] }];
-  
-//     Plotly.newPlot("plot", data);
-//   }
-  
-//   // Call updatePlotly() when a change takes place to the DOM
-//   d3.selectAll("#selDataset").on("change", updatePlotly);
-  
-//   // This function is called when a dropdown menu item is selected
-//   function updatePlotly() {
-//     // Use D3 to select the dropdown menu
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
-  
-//     // Initialize x and y arrays
-//     var x = [];
-//     var y = [];
-  
-//     if (dataset === 'dataset1') {
-//       x = [1, 2, 3, 4, 5];
-//       y = [1, 2, 4, 8, 16];
+//     // List of meta data for the selected person ID
+//     for (const [key, value] of Object.entries(person)) {
+//         d3.select("ul").append("li").text(`${key}: ${value}`);
 //     }
+
+//     updateBar(newdata);
+//     updateBubble(newdata);
+// };
   
-//     if (dataset === 'dataset2') {
-//       x = [10, 20, 30, 40, 50];
-//       y = [1, 10, 100, 1000, 10000];
-//     }
+// function updateBar(newdata) {
+//     // Make the bar chart of the top ten bacteria from the person ID
+//     // Slice the first 10 objects for plotting
+//     let slicedData = sample_values.slice(0, 10);
+//     let slicedIDs = otu_ids.slice(0, 10);
+
+//     // Reverse the array to accommodate Plotly's defaults
+//     let reversedData = slicedData.reverse();
+//     let reversedIDs = slicedIDs.reverse();
+//     reversedIDs = reversedIDs.map(id=>"OTU " + id.toString());
+
+//     // Trace for the Bar Data
+//     var barTrace = {
+//         x: reversedData,
+//         y: reversedIDs,
+//         text: reversedIDs,
+//         name: "BB Bacteria",
+//         type: "bar",
+//         orientation: "h"
+//     };
+    
+//     // Bar Data
+//     var barData = [barTrace];
+    
+//     // Apply the group bar mode to the layout
+//     var barLayout = {
+//         title: "Top Ten Bacteri",
+//     };
+
+//     Plotly.newPlot("bar", barData, barLayout);
+// };
+
+// function updateBubble(newdata) {
+//    // Make the bubble chart of the top ten bacteria  from the person ID
+//     var bubbleTrace = {
+//         x: otu_ids,
+//         y: sample_values,
+//         text: otu_labels,
+//         mode: 'markers',
+//         marker: {
+//         color: otu_ids,
+//         size: sample_values
+//         }
+//     };
+    
+//     var bubbleData = [bubbleTrace];
+    
+//     var bubbleLayout = {
+//         title: 'Count of Bacteria Strains',
+//         showlegend: false,
+//         height: 600,
+//         width: 600
+//     };
+
+//     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+// };
+
+// // On change to the DOM, call getData()
+// // d3.selectAll("#selDataset").on("change", getData);
+
+// // function getData() {
+// //     var dropdownMenu = d3.select("#selDataset");
+// //     // Assign the value of the dropdown menu option to a variable
+// //     var personID = dropdownMenu.property("value");
+
+// //     // Initialize an empty array for the person ID data
+// //     var personDataset = [];
+
+
+
+// // // Sort the array in descending order
+// // var numArray = [10, 22, 4];
+// // numArray.sort(function compareFunction(a, b) {
+// //   // resulting order is descending
+// //   return b - a;
+// // });
+
+// // // Return elements from indicies 0 to 9
+// // var topTen = bacteria.slice(0, 10);
+// // // Reverse the list for plotly to order corectly
+// // topTen.reverse();
+// // console.log(topTen);
+
+// //   // Call functions to update the charts
+// //     updateBar(personDataset);
+// //     updateBubble(personDataset);
+
+// // };
+
+
+// // // Update the restyled bar plot's values
+// // function updateBar(newdata) {
+// //     Plotly.restyle("bar", "values", [newdata]);
+// //   }
+// // // Update the restyled bubble plot's values
+// // function updateBubble(newdata) {
+// //     Plotly.restyle("bubble", "values", [newdata]);
+// //   }
+
+// // };
+
+
+// // function init() {
+// //     data = [{
+// //       x: [1, 2, 3, 4, 5],
+// //       y: [1, 2, 4, 8, 16] }];
   
-//     // Note the extra brackets around 'x' and 'y'
-//     Plotly.restyle("plot", "x", [x]);
-//     Plotly.restyle("plot", "y", [y]);
-//   }
+// //     Plotly.newPlot("plot", data);
+// //   }
   
-init();
+// //   // Call updatePlotly() when a change takes place to the DOM
+// //   d3.selectAll("#selDataset").on("change", updatePlotly);
+  
+// //   // This function is called when a dropdown menu item is selected
+// //   function updatePlotly() {
+// //     // Use D3 to select the dropdown menu
+// //     var dropdownMenu = d3.select("#selDataset");
+// //     // Assign the value of the dropdown menu option to a variable
+// //     var dataset = dropdownMenu.property("value");
+  
+// //     // Initialize x and y arrays
+// //     var x = [];
+// //     var y = [];
+  
+// //     if (dataset === 'dataset1') {
+// //       x = [1, 2, 3, 4, 5];
+// //       y = [1, 2, 4, 8, 16];
+// //     }
+  
+// //     if (dataset === 'dataset2') {
+// //       x = [10, 20, 30, 40, 50];
+// //       y = [1, 10, 100, 1000, 10000];
+// //     }
+  
+// //     // Note the extra brackets around 'x' and 'y'
+// //     Plotly.restyle("plot", "x", [x]);
+// //     Plotly.restyle("plot", "y", [y]);
+// //   }
+  
+// init();

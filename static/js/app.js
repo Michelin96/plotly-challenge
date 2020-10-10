ffb876c2d5a306c3e83be09c4def6d7fec6010cf
MichelleH <voirdire96@gmail.com>
2020-10-09 21:30:26 -0700

    Bar chart changes on select

--- static/js/app.js
+++ static/js/app.js
@@ -13,89 +13,125 @@ d3.json('samples.json').then(data => {
         .append("option")
         .attr("value", d => d)
         .text(d => d)
+        
 
+    // When a new item is selected, change the demographics and plots
     d3.selectAll("body").on("change", updateMetadata)
-    
+    d3.selectAll("body").on("change", updateBar)
+
+
     function init() {
-        // Default list of metadata using first personID
+        // Default list of demographics using first personID
         for (const [key, value] of Object.entries(metadata[0])) {
             d3.select("ul").append("li").text(`${key}: ${value}`);
         };
+
+        // Default bar chart using first personID
+        let slicedData = samples[0].sample_values.slice(0, 10);
+        let slicedIDs = samples[0].otu_ids.slice(0, 10);
+
+        // Reverse the array to accommodate Plotly's defaults
+        let reversedData = slicedData.reverse();
+        let reversedIDs = slicedIDs.reverse();
+        reversedIDs = reversedIDs.map(id=>"OTU " + id.toString());
+
+        // Trace for the Bar Data
+        var barTrace = {
+            x: reversedData,
+            y: reversedIDs,
+            text: reversedIDs,
+            name: "BB Bacteria",
+            type: "bar",
+            orientation: "h"
+        };
+        
+        // Bar Data
+        var barData = [barTrace];
+        
+        // Apply the group bar mode to the layout
+        var barLayout = {
+            title: "Top Ten Bacteri",
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+
+        // Default bubbele chart using first personID
+        var bubbleTrace = {
+            x: samples[0].otu_ids,
+            y: samples[0].sample_values,
+            text: samples[0].otu_labels,
+            mode: 'markers',
+            marker: {
+            color: samples[0].otu_ids,
+            size: samples[0].sample_values
+            }
+        };
+        
+        var bubbleData = [bubbleTrace];
+        
+        var bubbleLayout = {
+            title: 'Count of Bacteria Strains',
+            showlegend: false,
+            height: 600,
+            width: 600
+        };
+    
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    
     };
-
-    //     // Default plots using the first entries
-    //     updateBar(samples[0]);
-    //     updateBubble(samples[0])
-    // }
-    //console.log(metadata);
-
- 
-
-
 
     function updateMetadata() {
         let selectedID = d3.select("#selDataset").property("value")
-        
-        // Clear the metatdata list
+        // Clear the demographic list
         d3.select("ul").html("");
-
+        // Search metadata for a selection match and change demographics list
         for (item in metadata ){
-            console.log(metadata[item].id)
             if (metadata[item].id == selectedID){
-                console.log("Found a match")
                 for (const [key, value] of Object.entries(metadata[item])) {
                     d3.select("ul").append("li").text(`${key}: ${value}`);
                 };
             };
         };
     };
-        
-        // console.log(selectedID)
-        // for (let i = 0; i < metadata.length; i++){
-        //     console.log(metadata[i].id)
-        //     if (metadata[i].id === selectedID) {
-        //         console.log(metadata[i])
-        //         metadata.forEach(Object.entries(metadata[i]))
-        //             d3.select("ul").append("li").text(`${key}: ${value}`);
-        //         };
-        //     };
-        // };
-    
-// //     updateBar(newdata);
-// //     updateBubble(newdata);
-// // };
   
-// //  function updateBar(newdata) {
-//     // Make the bar chart of the top ten bacteria from the person ID
-//     // Slice the first 10 objects for plotting
-//     let slicedData = samples[0].sample_values.slice(0, 10);
-//     let slicedIDs = samples[0].otu_ids.slice(0, 10);
+    function updateBar() {
+        let selectedID = d3.select("#selDataset").property("value")
 
-//     // Reverse the array to accommodate Plotly's defaults
-//     let reversedData = slicedData.reverse();
-//     let reversedIDs = slicedIDs.reverse();
-//     reversedIDs = reversedIDs.map(id=>"OTU " + id.toString());
+        // Search metadata for a selection match and change demographics list
+        for (item in samples ){
+            if (samples[item].id == selectedID){
+                // Make the bar chart of the top ten bacteria from the person ID
+                // Slice the first 10 objects for plotting
+                let slicedData = samples[item].sample_values.slice(0, 10);
+                let slicedIDs = samples[item].otu_ids.slice(0, 10);
 
-//     // Trace for the Bar Data
-//     var barTrace = {
-//         x: reversedData,
-//         y: reversedIDs,
-//         text: reversedIDs,
-//         name: "BB Bacteria",
-//         type: "bar",
-//         orientation: "h"
-//     };
-    
-//     // Bar Data
-//     var barData = [barTrace];
-    
-//     // Apply the group bar mode to the layout
-//     var barLayout = {
-//         title: "Top Ten Bacteri",
-//     };
+                // Reverse the array to accommodate Plotly's defaults
+                let reversedData = slicedData.reverse();
+                let reversedIDs = slicedIDs.reverse();
+                reversedIDs = reversedIDs.map(id=>"OTU " + id.toString());
 
-//     Plotly.newPlot("bar", barData, barLayout);
-// // // };
+                // Trace for the Bar Data
+                var barTrace = {
+                    x: reversedData,
+                    y: reversedIDs,
+                    text: reversedIDs,
+                    name: "BB Bacteria",
+                    type: "bar",
+                    orientation: "h"
+                };
+                
+                // Bar Data
+                var barData = [barTrace];
+                
+                // Apply the group bar mode to the layout
+                var barLayout = {
+                    title: "Top Ten Bacteri",
+                };
+
+                Plotly.newPlot("bar", barData, barLayout);
+            };
+        };
+    };
 
 // // // function updateBubble(newdata) {
 // //    // Make the bubble chart of the top ten bacteria  from the person ID
